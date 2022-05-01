38eca6ed17bf994e922f3a88bab08736b3c05bef
danielabadgamero <danielabadgamero@gmail.com>
2022-05-01 10:00:36 +0200

    Added operation keys change colour when clicked

--- script.js
+++ script.js
@@ -30,37 +30,63 @@ buttons.forEach(button => {
                     num2 += button.id;
                     display.textContent = num2;
                 }
-            } else {
-                if (button.id === "equal") {
-                    switch (operator) {
-                        case "sum":
-                            display.textContent = Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000;
-                            num1 = (Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000).toString();
-                            num2 = "";
-                            break;
-                        case "substract":
-                            display.textContent = Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000;
-                            num1 = (Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000).toString();
-                            num2 = "";
-                            break;
-                        case "multiply":
-                            display.textContent = Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000;
-                            num1 = (Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000).toString();
-                            num2 = "";
-                            break;
-                        case "divide":
-                            display.textContent = Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000;
-                            num1 = (Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000).toString();
-                            num2 = "";
-                    }
-                } else if (button.id === "erase") {
-                    display.textContent = "";
-                    num1 = "";
-                    num2 = "";
-                    operator = "";
-                } else {
-                    operator = button.id;
+            } else if (button.id === "equal") {
+                document.getElementById(operator).classList.remove("operation-held");
+                switch (operator) {
+                    case "sum":
+                        display.textContent = Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "substract":
+                        display.textContent = Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "multiply":
+                        display.textContent = Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "divide":
+                        display.textContent = Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
                 }
+            } else if (button.id === "erase") {
+                document.getElementById(operator).classList.remove("operation-held");
+                display.textContent = "";
+                num1 = "";
+                num2 = "";
+                operator = "";
+            } else if (num2 === "") {
+                button.classList.add("operation-held");
+                operator = button.id;
+            } else {
+                document.getElementById(operator).classList.remove("operation-held");
+                switch (operator) {
+                    case "sum":
+                        display.textContent = Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "substract":
+                        display.textContent = Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "multiply":
+                        display.textContent = Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                        break;
+                    case "divide":
+                        display.textContent = Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000;
+                        num1 = (Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                        num2 = "";
+                }
+                operator = button.id;
+                button.classList.add("operation-held")
             }
         } else {
             if (!operator) {
