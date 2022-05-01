2f46d38750c891434ed0a0a742d01259d84bd5b9
danielabadgamero <danielabadgamero@gmail.com>
2022-05-01 09:35:33 +0200

    Working on display.textContent.length constraint

--- script.js
+++ script.js
@@ -73,3 +73,72 @@ buttons.forEach(button => {
         }
     })
 })
+
+display.addEventListener('change', () => {
+    if (display.textContent.length > 10) {
+        buttons.forEach(button => {
+            if (parseInt(buttons.id) || buttons.id === ".") {
+                button.removeEventListener('click', () => {
+                    if (!parseInt(button.id)) {
+                        if (button.id === ".") {
+                            if (!operator) {
+                                num1 += button.id;
+                                display.textContent = num1;
+                            } else {
+                                num2 += button.id;
+                                display.textContent = num2;
+                            }
+                        } else if (button.id === "0") {
+                            if (!operator) {
+                                num1 += button.id;
+                                display.textContent = num1;
+                            } else {
+                                num2 += button.id;
+                                display.textContent = num2;
+                            }
+                        } else {
+                            if (button.id === "equal") {
+                                switch (operator) {
+                                    case "sum":
+                                        display.textContent = Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000;
+                                        num1 = (Math.round((parseFloat(num1, 10) + parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                                        num2 = "";
+                                        break;
+                                    case "substract":
+                                        display.textContent = Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000;
+                                        num1 = (Math.round((parseFloat(num1, 10) - parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                                        num2 = "";
+                                        break;
+                                    case "multiply":
+                                        display.textContent = Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000;
+                                        num1 = (Math.round((parseFloat(num1, 10) * parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                                        num2 = "";
+                                        break;
+                                    case "divide":
+                                        display.textContent = Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000;
+                                        num1 = (Math.round((parseFloat(num1, 10) / parseFloat(num2, 10)) * 1000000) / 1000000).toString();
+                                        num2 = "";
+                                }
+                            } else if (button.id === "erase") {
+                                display.textContent = "";
+                                num1 = "";
+                                num2 = "";
+                                operator = "";
+                            } else {
+                                operator = button.id;
+                            }
+                        }
+                    } else {
+                        if (!operator) {
+                            num1 += button.id;
+                            display.textContent = num1;
+                        } else {
+                            num2 += button.id;
+                            display.textContent = num2;
+                        }
+                    }
+                })
+            }
+        }
+    )}
+})
