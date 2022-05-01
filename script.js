2186cb8876dd5910a27d1e61db9d349f578ae46d
danielabadgamero <danielabadgamero@gmail.com>
2022-05-01 08:45:29 +0200

    Added number buttons click event listeners

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const display = document.querySelector(".display");
 const buttons = Array.from(document.querySelectorAll("button"));
+
+let num1 = "";
+let num2 = "";
+let operator = "";
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (!operator) {
+            num1 += button.id
+        } else {
+            num2 += button.id
+        }
+    })
+})
