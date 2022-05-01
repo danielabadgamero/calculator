0561337424d5002f345d1d2140123cad276a81fd
danielabadgamero <danielabadgamero@gmail.com>
2022-05-01 09:01:00 +0200

    Added sum operation and display style

--- script.js
+++ script.js
@@ -7,10 +7,31 @@ let operator = "";
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        if (!operator) {
-            num1 += button.id
+        if (!parseInt(button.id)) {
+            if (button.id === ".") {
+                if (!operator) {
+                    num1 += button.id;
+                } else {
+                    num2 += button.id;
+                }
+            } else {
+                if (button.id === "equal") {
+                    switch (operator) {
+                        case "sum":
+                            display.textContent = parseInt(num1, 10) + parseInt(num2, 10);
+                    }
+                } else {
+                    operator = button.id;
+                }
+            }
         } else {
-            num2 += button.id
+            if (!operator) {
+                num1 += button.id;
+                display.textContent = num1;
+            } else {
+                num2 += button.id;
+                display.textContent = num2;
+            }
         }
     })
 })
